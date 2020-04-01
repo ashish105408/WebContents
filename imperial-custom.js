a09aaa1e1fcdcadaf3d9ac4a9b302ac062bc86ec
ashish105408 <ashish105408@users.noreply.github.com>
2020-04-01 13:01:56 +0530

    Update imperial-custom.js
    Added logic to change the label name.

--- imperial-custom.js
+++ imperial-custom.js
@@ -1,3 +1,5 @@
 $( document ).ready(function() {
     console.log( "This is a custom Java Script code for imperial by Ashish Nanda!" );
+    var termlookupelemnt = document.getElementById("elcn_anticipatedentrytermid");
+    termlookupelemnt.parentElement.firstElementChild.innerHTML = "Start Term 2";
 });
