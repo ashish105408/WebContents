f0fed588477a5bfa2ce5ce1989aa90cd13daf981
ashish105408 <ashish105408@users.noreply.github.com>
2020-04-02 10:17:39 +0530

    Delete imperial-custom1.js

--- imperial-custom1.js
+++
@@ -1,5 +0,0 @@
-$( document ).ready(function() {
-    console.log( "This is a custom Java Script code for imperial by Ashish Nanda!" );
-    var termlookupelemnt = document.getElementById("elcn_anticipatedentrytermid");
-    termlookupelemnt.parentElement.firstElementChild.innerHTML = "Start Term 2";
-});
