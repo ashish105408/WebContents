5436b83a15323a710fbc831b4d751e01477887ba
ashish105408 <ashish105408@users.noreply.github.com>
2021-06-26 21:23:12 +0530

    Update elluciancustomprogram.js

--- elluciancustomprogram.js
+++ elluciancustomprogram.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-    console.log( "This is a custom Java Script code for imperial by Ashish Nanda test111!" );
+    console.log( "This is a custom Java Script code for imperial by Ashish Nanda test1113!" );
     $("#suss_residencypassexpirydate").on("change", function (e) {
 		
 			var selecteddate = $("#suss_residencypassexpirydate").val().split("/");
